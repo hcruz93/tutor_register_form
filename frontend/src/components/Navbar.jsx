9d6e80354b5d7aae8b5c7417ba629cd0d144ede9
Humberto <hum.alex93@gmail.com>
2026-08-21 10:18:18 -0600

    Install react frontend and configuration

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom'
+
+function Navbar() {
+  return (
+    <nav>
+      <Link to="/">Inicio</Link> | <Link to="/about">Acerca de</Link>
+    </nav>
+  )
+}
+
+export default Navbar
